06a78df62a3d5de5305289101c36f306beb71c54
gagatpadi <padiyogaradhana@gmail.com>
2024-03-07 19:49:09 +0700

    commit 2

--- project 1/project 1.cpp
+++ project 1/project 1.cpp
@@ -28,3 +28,17 @@ void input() {
 	
 }
 
+void bubbleShortArray() {
+	int pass = 1;
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j ++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1;
+
+		cout << "\nPass " << pass - 1 << "; ";
