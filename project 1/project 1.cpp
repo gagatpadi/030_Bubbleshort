dde95ac9d57408b61f8098e7512f51d118c5e394
gagatpadi <padiyogaradhana@gmail.com>
2024-03-07 19:49:37 +0700

    commit 3

--- project 1/project 1.cpp
+++ project 1/project 1.cpp
@@ -4,29 +4,7 @@ using namespace std;
 int arr[20];
 int n;
 
-void input() {
-	while (true) {
-		cout << "Masukan banyaknya alasan pada array : ";
-		cin >> n;
-		if (n <= 20)
-			break;
-		else {
-			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
-		}
-		
-	}
 
-		cout << endl;
-		cout << "====================" << endl;
-		cout << "Masukan Elemen Array" << endl;
-		cout << "====================" << endl;
-
-		for (int i = 0; i < n; i++) {
-			cout << "Data ke-" << (i + 1) << "; ";
-			cin >> arr[i];
-		}
-	
-}
 
 void bubbleShortArray() {
 	int pass = 1;
@@ -42,3 +20,38 @@ void bubbleShortArray() {
 		pass = pass + 1;
 
 		cout << "\nPass " << pass - 1 << "; ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";
+
+		}
+		cout << endl;
+
+	} while (pass <= n - 1);    //step 5
+}
+
+void display() {
+	cout << endl;
+	cout << "============================" << endl;
+	cout << "ELement Array yang telah tersusun" << endl;
+	cout << "============================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " -->";
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
+
+int main() {
+	input();   // call the input function to get user input
+	bubbleShortArray();  //call the selection sort function
+	display();  // display the sorted array
+	system("pause");
+
+	return 0;
+}
